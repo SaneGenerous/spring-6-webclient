3047374bb8321a61486206c6967c15cc7173668a
Sane Generous <karim2k11@gmail.com>
2024-05-15 12:01:25 +0100

    Get Beers List as a Map

--- src/main/java/tp/msk/spring6webclient/client/BeerClient.java
+++ src/main/java/tp/msk/spring6webclient/client/BeerClient.java
@@ -2,6 +2,9 @@ package tp.msk.spring6webclient.client;
 
 import reactor.core.publisher.Flux;
 
+import java.util.Map;
+
 public interface BeerClient {
     Flux<String> listBeers();
+    Flux<Map> listBeersMap();
 }
--- src/test/java/tp/msk/spring6webclient/client/BeerClientImplTest.java
+++ src/test/java/tp/msk/spring6webclient/client/BeerClientImplTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -12,13 +15,25 @@ class BeerClientImplTest {
     @Autowired
     BeerClient client;
 
+
+
+    @Test
+    void testListBeersMap() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        client.listBeersMap().subscribe(response -> {
+            System.out.println(response);
+            atomicBoolean.set(true);
+        });
+        await().untilTrue(atomicBoolean);
+    }
+
     @Test
     void listBeers() {
-        client.listBeers().subscribe(System.out::println);
-        try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        client.listBeers().subscribe(response -> {
+            System.out.println(response);
+            atomicBoolean.set(true);
+        });
+        await().untilTrue(atomicBoolean);
     }
 }
