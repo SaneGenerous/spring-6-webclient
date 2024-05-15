711b844f9f17ff3cbe7ec5c32102ea32c5e60054
Sane Generous <karim2k11@gmail.com>
2024-05-15 13:10:03 +0100

    Implement and test getBeerByBeerStyle

--- src/main/java/tp/msk/spring6webclient/client/BeerClient.java
+++ src/main/java/tp/msk/spring6webclient/client/BeerClient.java
@@ -1,13 +1,11 @@
 package tp.msk.spring6webclient.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tp.msk.spring6webclient.model.BeerDTO;
 
 import java.util.Map;
-import java.util.concurrent.Flow;
 
 public interface BeerClient {
     Flux<String> listBeers();
@@ -15,4 +13,5 @@ public interface BeerClient {
     Flux<JsonNode> listBeersJsonNode();
     Flux<BeerDTO> listBeerDtos();
     Mono<BeerDTO> getBeerById(String id);
+    Flux<BeerDTO> getBeerByBeerStyle(String beerStyle);
 }
